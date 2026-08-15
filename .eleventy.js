804e3c85bc310773add25b70527c7df14594fc4e
dorianstuffs <dorianmoes@proton.me>
2026-08-14 19:16:02 -0500

    Updated .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,4 @@
 module.exports = function (eleventyConfig) {
 
   eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
-
-  return {
-    // This makes sure HTML files use Nunjucks
-    dir: {
-      output: "public",
-    },
-  };
 };
