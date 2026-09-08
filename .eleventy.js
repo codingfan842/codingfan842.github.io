e82b1522d2f3c8e7663301497bed7c255fed217e
dorianstuffs <dorianmoes@proton.me>
2026-09-08 17:48:04 -0500

    Added eleventy

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("src");
+    return {
+        markdownTemplateEngine: "njk"
+    };
+};
+  return {
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    templateFormats: ["md", "njk", "html"],
+    dir: {
+      input: ".",
+      output: "_site"
+    }
+  };
